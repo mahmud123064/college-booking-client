f496d65ca0bf44038472d727bafba39cbd107f3b
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-07-24 17:04:33 +0600

    Admission now section added

--- src/Pages/Home/CallUs/CallUs.jsx
+++ src/Pages/Home/CallUs/CallUs.jsx
@@ -5,7 +5,6 @@ const CallUs = () => {
         <div className="callus rounded-xl grid place-content-center mb-12">
             <div>
                 <h2 className="text-center text-white text-2xl mb-3">Do You Want Admission?</h2>
-                {/* <h2 className="text-center text-white text-2xl">Call Us: +88 01759 733 296</h2> */}
                 <Link to= '/admission'><button className="btn btn-accent"> Admission Now</button></Link>
             </div>
         </div>
